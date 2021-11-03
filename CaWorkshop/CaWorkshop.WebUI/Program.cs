4d962472caa76e5a0096e7e48e2ae7ba3829e118
Jason Taylor <jasontaylor@ssw.com.au>
2021-11-03 12:04:28 +1100

    GettingStarted-02

--- CaWorkshop/CaWorkshop.WebUI/Program.cs
+++ CaWorkshop/CaWorkshop.WebUI/Program.cs
@@ -1,16 +1,18 @@
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
-using Microsoft.EntityFrameworkCore;
-using CaWorkshop.WebUI.Data;
+﻿using CaWorkshop.WebUI.Data;
 using CaWorkshop.WebUI.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
+builder.Services.AddScoped<ApplicationDbContextInitialiser>();
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
@@ -26,6 +28,25 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    try
+    {
+        var initialiser = scope.ServiceProvider
+            .GetRequiredService<ApplicationDbContextInitialiser>();
+        initialiser.Update();
+        initialiser.Seed();
+    }
+    catch (Exception ex)
+    {
+        var logger = scope.ServiceProvider
+            .GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex,
+            "An error occurred during database initialisation.");
+        throw;
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
