08a314eedc5b924be972f21177d3079569034ef8
Jason Taylor <jasontaylor@ssw.com.au>
2021-11-04 10:18:29 +1100

    BeyondBasics-03

--- CaWorkshop/CaWorkshop.Application/TodoLists/Queries/GetTodoLists/GetTodoLists.cs
+++ CaWorkshop/CaWorkshop.Application/TodoLists/Queries/GetTodoLists/GetTodoLists.cs
@@ -1,4 +1,6 @@
-﻿using CaWorkshop.Application.Common.Models;
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using CaWorkshop.Application.Common.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace CaWorkshop.Application.TodoLists.Queries.GetTodoLists;
@@ -11,10 +13,14 @@ public class GetTodoListsQueryHandler
     : IRequestHandler<GetTodoListsQuery, TodosVm>
 {
     private readonly IApplicationDbContext _context;
+    private readonly IMapper _mapper;
 
-    public GetTodoListsQueryHandler(IApplicationDbContext context)
+    public GetTodoListsQueryHandler(
+        IApplicationDbContext context,
+        IMapper mapper)
     {
         _context = context;
+        _mapper = mapper;
     }
 
     public async Task<TodosVm> Handle(
@@ -32,7 +38,7 @@ public class GetTodoListsQueryHandler
                  })
                  .ToList(),
             Lists = await _context.TodoLists
-                 .Select(TodoListDto.Projection)
+                 .ProjectTo<TodoListDto>(_mapper.ConfigurationProvider)
                  .ToListAsync(cancellationToken)
         };
     }
