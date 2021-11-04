dd02d50efafd23f96df56de5bc57a04262347b17
Jason Taylor <jasontaylor@ssw.com.au>
2021-11-04 17:00:18 +1100

    AdvancedTopics-04

--- CaWorkshop/CaWorkshop.WebUI/Program.cs
+++ CaWorkshop/CaWorkshop.WebUI/Program.cs
@@ -1,64 +1,43 @@
-﻿using CaWorkshop.Application;
-using CaWorkshop.Infrastructure;
-using CaWorkshop.Infrastructure.Data;
-using CaWorkshop.WebUI;
+﻿using CaWorkshop.Infrastructure.Data;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace CaWorkshop.WebUI;
 
-// Add services to the container.
-builder.Services.AddInfrastructureServices(builder.Configuration);
-builder.Services.AddApplicationServices();
-builder.Services.AddWebUIServices();
-
-var app = builder.Build();
-
-using (var scope = app.Services.CreateScope())
+public class Program
 {
-    try
+    public static void Main(string[] args)
     {
-        var initialiser = scope.ServiceProvider
-            .GetRequiredService<ApplicationDbContextInitialiser>();
-        initialiser.Update();
-        initialiser.Seed();
+        var host = CreateHostBuilder(args).Build();
+
+        using (var scope = host.Services.CreateScope())
+        {
+            try
+            {
+
+                var initialiser = scope.ServiceProvider
+                    .GetRequiredService<ApplicationDbContextInitialiser>();
+
+                initialiser.Update();
+                initialiser.Seed();
+            }
+            catch (Exception ex)
+            {
+                var logger = scope.ServiceProvider
+                    .GetRequiredService<ILogger<Program>>();
+
+                logger.LogError(ex,
+                    "An error occurred during database initialisation.");
+
+                throw;
+            }
+        }
+
+        host.Run();
     }
-    catch (Exception ex)
-    {
-        var logger = scope.ServiceProvider
-            .GetRequiredService<ILogger<Program>>();
-        logger.LogError(ex,
-            "An error occurred during database initialisation.");
-        throw;
-    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
 }
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseMigrationsEndPoint();
-}
-else
-{
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseOpenApi();
-app.UseSwaggerUi3();
-
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseIdentityServer();
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action=Index}/{id?}");
-app.MapRazorPages();
-
-app.MapFallbackToFile("index.html");
-
-app.Run();
---
+++ CaWorkshop/CaWorkshop.WebUI/Startup.cs
@@ -0,0 +1,61 @@
+﻿using CaWorkshop.Application;
+using CaWorkshop.Application.Common.Interfaces;
+using CaWorkshop.Infrastructure;
+using CaWorkshop.WebUI.Filters;
+using CaWorkshop.WebUI.Services;
+using NSwag;
+using NSwag.Generation.Processors.Security;
+
+namespace CaWorkshop.WebUI;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddInfrastructureServices(Configuration);
+        services.AddApplicationServices();
+        services.AddWebUIServices();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        // Configure the HTTP request pipeline.
+        if (env.IsDevelopment())
+        {
+            app.UseMigrationsEndPoint();
+        }
+        else
+        {
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        //app.UseOpenApi();
+        app.UseSwaggerUi3();
+
+        app.UseAuthentication();
+        app.UseIdentityServer();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller}/{action=Index}/{id?}");
+            endpoints.MapRazorPages();
+            endpoints.MapFallbackToFile("index.html");
+        });
+    }
+}
